42410e7df341f6cf92fabe9fcb964eeda9b341a7
esraaakgull <eesraakgull@gmail.com>
2023-12-26 20:56:02 +0300

    Button component is made and published as a package

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'styled-buttons-package'
 import 'styled-buttons-package/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent type="dashed" />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useEffect, useState } from 'react'
+import { Button } from 'antd'
+import './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({ type }) => {
+  const [text, setText] = useState('')
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  useEffect(() => {
+    const capitalizedType = capitalizeFirstLetter(type)
+    setText(`${capitalizedType}  Button`)
+  }, [])
+
+  return (
+    <Button type={type} className='btn' style={{ margin: '10px' }}>
+      {text}
+    </Button>
+  )
 }
